a8199c82d21ddf9ca7d0af15494a7b288a5f7114
Kevin Fields <kfields31@gmail.com>
2022-03-28 15:49:44 -0500

    final console cleaning

---
+++ src/store/values.js
@@ -0,0 +1,3 @@
+export const values = [
+  'Aaron Allenson', 'Adam Faris', 'Austin Hollett', 'Amanda Knowles', 'Alexander Pool', 'Alexis Whight', 'Bradley Crow', 'Britney Phillips', 'Blanche Ratter', 'Brian Sanders', 'Burly Mann', 
+];
